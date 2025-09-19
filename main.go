e1bf4ef804d8aaf14f8efc7aab2deea18490a8c8
Uzzee <kudrinski199iii@gmail.com>
2025-09-19 20:00:14 +0300

    3-struct

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"cli/app/storage"
+	"fmt"
+	"time"
+)
 
+func main() {
+	binList := storage.NewBinList()
+	bin, err := binList.NewBin("hello", true, time.Now(), "bye")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	binList.AddBin(*bin)
+	binList.ReadBinList("data.json")
 }
